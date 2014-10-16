2d4cb131e35d800c8140f08bb285d332045297f2
hackiftekhar <hack.iftekhar@gmail.com>
2014-10-16 20:12:30 +0530

    Added delegate, Project restructured

---
+++ IQDropDownTextField Demo/Drop Down TextField/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  Drop Down TextField
+//
+//  Created by hp on 10/11/13.
+//  Copyright (c) 2013 Iftekhar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIToolbar *toolbar = [[UIToolbar alloc] init];
+    [toolbar setBarStyle:UIBarStyleBlackTranslucent];
+    [toolbar sizeToFit];
+    UIBarButtonItem *buttonflexible = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    UIBarButtonItem *buttonDone = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneClicked:)];
+    
+    [toolbar setItems:[NSArray arrayWithObjects:buttonflexible,buttonDone, nil]];
+    textFieldDatePicker.inputAccessoryView = toolbar;
+    textFieldTextPicker.inputAccessoryView = toolbar;
+    textFieldTimePicker.inputAccessoryView = toolbar;
+    textFieldOptionalTextPicker.inputAccessoryView = toolbar;
+    
+    textFieldTextPicker.isOptionalDropDown = NO;
+    [textFieldTextPicker setItemList:[NSArray arrayWithObjects:@"London",@"Johannesburg",@"Moscow",@"Mumbai",@"Tokyo",@"Sydney", nil]];
+    [textFieldOptionalTextPicker setItemList:[NSArray arrayWithObjects:@"London",@"Johannesburg",@"Moscow",@"Mumbai",@"Tokyo",@"Sydney", nil]];
+
+    [textFieldDatePicker setDropDownMode:IQDropDownModeDatePicker];
+    
+    [textFieldTimePicker setDropDownMode:IQDropDownModeTimePicker];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)doneClicked:(UIBarButtonItem*)button
+{
+    [self.view endEditing:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
